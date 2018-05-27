40261f2f2232b10c97a04f66503757b263a28823
wangwei <429744781@qq.com>
2018-05-27 16:48:27 +0800

    举一反三 支持泛型

--- src/com/designPattern/factory/FactoryPatternDemo.java
+++ src/com/designPattern/factory/FactoryPatternDemo.java
@@ -1,13 +1,15 @@
 package com.designPattern.factory;
 
+import org.w3c.dom.css.Rect;
+
 public class FactoryPatternDemo {
     public static void main(String[] args) {
         ShapeFactory shapeFactory=new ShapeFactory();
-        Shape shape1=shapeFactory.getShape("circle");
+        Shape shape1=shapeFactory.getShape(Circle.class);
         shape1.draw();
-        Shape shape2=shapeFactory.getShape("rectangle");
+        Shape shape2=shapeFactory.getShape(Rectangle.class);
         shape2.draw();
-        Shape shape3=shapeFactory.getShape("square");
+        Shape shape3=shapeFactory.getShape(Square.class);
         shape3.draw();
     }
 }
--- src/com/designPattern/factory/ShapeFactory.java
+++ src/com/designPattern/factory/ShapeFactory.java
@@ -1,16 +1,19 @@
 package com.designPattern.factory;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ShapeFactory {
-    public Shape getShape(String shapeType){
-        if(shapeType==null){
-            return null;
-        }
-        if(shapeType.equalsIgnoreCase("circle")){
-            return new Circle();
-        }else if(shapeType.equalsIgnoreCase("rectangle")){
-            return new Rectangle();
-        }else if(shapeType.equalsIgnoreCase("square")){
-            return new Square();
+    public Shape getShape(Class<?> clazz){
+        try {
+            return (Shape)clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
         }
         return null;
     }
